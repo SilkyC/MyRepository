bfb566c89b323bb70e86453149f6a086c66c175c
Silky Chathukutty <silky.c@gmail.com>
2019-10-27 11:14:05 +0530

    Beginners guide to JS

---
+++ VisualCodeProjects/js-Basics/dynamic.js
@@ -0,0 +1,14 @@
+//Unlike static language, the type of variable is decided on runtime
+
+//typeof is a reserved keyword
+
+//no floating or integers... all are numbers
+
+//undefined can also be a value when it is also a type
+
+let petName = 'Minu';
+let years = 09;
+let isAllowed = false;
+let schoolName = undefined;
+let selectedHouse = null;
+console.log('Dynamic script was executed');
---
+++ VisualCodeProjects/js-Basics/referenceObjects.js
@@ -0,0 +1,11 @@
+
+//Rather than defining each parameter of a single body
+let name = 'Silky';
+let age =40;
+
+
+//A cleaner code practice
+let person = {
+    name: 'Silky',
+    age: 40
+}
