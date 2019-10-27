11be23ecdbe51f900fa43f7aca5ca991d1f3702f
Silky Chathukutty <silky.c@gmail.com>
2019-10-27 15:39:00 +0530

    Ojects Introduction

--- VisualCodeProjects/js-Basics/referenceObjects.js
+++ VisualCodeProjects/js-Basics/referenceObjects.js
@@ -1,11 +1,19 @@
 //Object Literal
 //Rather than defining each parameter of a single body
-let name = 'Silky';
-let age =40;
+    // let name = 'Silky';
+    // let age =40;
 
 
-//A cleaner code practice to club multiple variables
+//STYLE 1: A cleaner code practice to club multiple variables
 let person = {
     name: 'Silky',
     age: 40
-}
+};
+
+//STYLE 2: Dot Notation
+person.name = 'Mrinal';
+
+//STYLE 3: BRACKET NOTATION
+//person['name'] = 'Minu';
+
+console.log(person.name);
