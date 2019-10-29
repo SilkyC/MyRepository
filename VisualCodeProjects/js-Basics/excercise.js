f2390b87d689db550f5df87cc171ccb76f0d49d5
Silky Chathukutty <silky.c@gmail.com>
2019-10-29 22:22:51 +0530

    --- Auto-Git Commit ---

---
+++ VisualCodeProjects/js-Basics/excercise.js
@@ -0,0 +1,9 @@
+let abc = 'red';
+let xyz = 'blue';
+
+let abc = 'blue';
+let xyz = 'pink';
+
+
+console.log(abc);
+console.log(xyz);
---
+++ VisualCodeProjects/js-Basics/operPrecedence.js
@@ -0,0 +1,10 @@
+//In general we go by BODMAS but with complex programing its good to use ()
+
+let z = 2 + 3 * 4;
+
+console.log(z);
+
+
+let w = (2 + 3) * 4;
+
+console.log(w);
