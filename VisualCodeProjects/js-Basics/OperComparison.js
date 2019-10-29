ba1f0e76ac3dae16a67ed2589e6e85d7273167d5
Silky Chathukutty <silky.c@gmail.com>
2019-10-29 21:22:28 +0530

    --- Auto-Git Commit ---

---
+++ VisualCodeProjects/js-Basics/OperComparison.js
@@ -0,0 +1,22 @@
+let n = 1;
+
+//Relational Operator
+console.log(n > 0);
+console.log(n >= 1);
+console.log(n < 1);
+console.log(n <= 1);
+
+//Equality Operator
+console.log(n === 1); //Strict Equality Operator (Type + Value)
+console.log('1' === 1); //String !== Number
+
+console.log(n === 1); //Loose Equality Operator (Only Value)
+console.log('1' === 1);
+console.log(true === 1);
+
+console.log(n !== 1);
+
+
+//Ternary Operator
+let points = 110;
+let  =
