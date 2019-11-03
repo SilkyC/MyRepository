f5dcb6279d41e6fa16a2eabb504ce8273eef5572
Silky Chathukutty <silky.c@gmail.com>
2019-11-03 17:42:52 +0530

    --- Auto-Git Commit ---

--- VisualCodeProjects/js-Basics/loops.js
+++ VisualCodeProjects/js-Basics/loops.js
@@ -1,15 +1,19 @@
 //#4 Infinite loop (Do not unleash the k9s below as they could tear apart the browser)
- let k = 0;
+// let k = 0;
 // while (k < 5) {
-     console.log(k);
-//     k++;
+//  console.log(k);
+//  k++;
 // }
 
 //#6 Break and continue
-  let l = 0;
-//while (l <= 10) {
-    //if (l === 5) break;
+let l = 0;
+while (l <= 10) {
+  // if (l === 5) break;
+  if (l % 2 === 0) {
+    l++;
+    continue;
+  }
 
-    console.log(l);
-//     l++; 
-//}
+  console.log(l);
+  l++;
+}
