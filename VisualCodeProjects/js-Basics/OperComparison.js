89482851e04135fefaa6c6d1eb9278e702fca0a4
Silky Chathukutty <silky.c@gmail.com>
2020-01-04 19:56:08 +0530

    spellcheck and OperConver. L13 error fix

--- VisualCodeProjects/js-Basics/OperComparison.js
+++ VisualCodeProjects/js-Basics/OperComparison.js
@@ -10,16 +10,16 @@ console.log(o <= 1);
 console.log(o === 1); //Strict Equality Operator (Type + Value)
 console.log('1' === 1); //String !== Number
 
-console.log(n === 1); //Loose Equality Operator (Only Value)
-console.log('1' === 1);
-console.log(true === 1);
+console.log(n === 2); //Loose Equality Operator (Only Value)
+console.log('2' === 2);
+console.log(true === 2);
 
-console.log(o !== 1);
+console.log(o !== 2);
 
 
 //Ternary or Conditional Operator
 // If a Customer has more than 100 points
-// they are 'platinum' customer, otherwsie,
+// they are 'platinum' customer, otherwise,
 // they are a 'gold' customer
 
 let points = 110;
