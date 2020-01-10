adc6c113f16a414b2b3e6f1c79a8dad691c1d265
Silky Chathukutty <silky.c@gmail.com>
2020-01-10 20:56:54 +0530

    --- Auto-Git Commit ---

--- VisualCodeProjects/js-Basics/exercises.js
+++ VisualCodeProjects/js-Basics/exercises.js
@@ -72,7 +72,7 @@ function checkSpeed(speed) {
         console.log('All good');
         return;
     }
-    const points = Math.floor((speed - speedLimit) / KmPerPoint)
+    const points = Math.floor((speed - speedLimit) / KmPerPoint);
     if (points >= 15)
         console.log('Suspend License! -', points, ' point(s)');
     else
@@ -94,13 +94,13 @@ function showNumbers(limit) {
             console.log(num, ' - EVEN Number');
 
         else
-            console.log(num, ' - ODD Number')
+            console.log(num, ' - ODD Number');
     }
 }
 
 //EXAMPLE #6
 //Count truthy
-const array = [0, null, undefined, NaN, '', 0, 2, 3]
+const array = [0, null, undefined, NaN, '', 0, 2, 3];
 
 console.log(countTruthy(array));
 
@@ -113,10 +113,8 @@ function countTruthy(array) {
 
 }
 
-
 //EXAMPLE #7
 //Show properties of object
-
 const movie = {
     title: 'abc',
     releaseYear: 2019,
@@ -135,7 +133,6 @@ function showProperties(obj) {
 
 //EXAMPLE #8
 //SUM of Multiples of 3 & 5 between 1-to-10 (SUM OF 3,6,9 & 5,10)
-
 console.log(sum(10));
 
 function sum(limit) {
@@ -148,21 +145,16 @@ function sum(limit) {
     return sum;
 }
 
-
 //EXAMPLE #9
 //Grading
-
-const array = [90, 80, 75, 65, 100];
-
+const arrays = [90, 80, 75, 65, 100];
 //Average: 70
 //0-59: F
 //60-69: D
 //70-79: C
 //80-89: B
 //90-100: A
-
-
-console.log(calculateGrade(array));
+console.log(calculateGrade(arrays));
 /*
 function calculateGrade(marks) {
     let sum = 0;
@@ -178,7 +170,6 @@ function calculateGrade(marks) {
 }
 */
 //Split Calculation and Comparison and create a function for calc
-
 function calculateGrade(marks) {
     const average = calculateAverage(marks);
     if (average < 60) return 'F';
@@ -188,21 +179,22 @@ function calculateGrade(marks) {
     return 'A';
 }
 
-function calculateAverage(array) {
+function calculateAverage(arrays) {
     let sum = 0;
-    for (let value of array)
+    for (let value of arrays)
         sum += value;
-    return sum / array.length;
+    return sum / arrays.length;
 }
-
 
 //EXAMPLE #10
 //Stars
-
 showStars(10);
 
 function showStars(rows) {
-    for (let row = 1; row <= rows; row++); {
-        let pattern
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let s = 0; s < row; s++)
+        pattern += '*';
+        console.log(pattern);
     }
 }
