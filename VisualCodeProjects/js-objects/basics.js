a447b21a4a8f1624d6f474af8b3bb22c3498f3cb
Silky Chathukutty <silky.c@gmail.com>
2020-04-19 16:24:30 +0530

    --- Auto-Git Commit ---

--- VisualCodeProjects/js-objects/basics.js
+++ VisualCodeProjects/js-objects/basics.js
@@ -1,33 +1,41 @@
 //Object-oiented Programming (OOP)
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function() {
-        console.log('draw');
-    }
-};
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+//     draw: function() {
+//         console.log('draw');
+//     }
+// };
 
-circle.draw();
+// circle.draw();
 
 
 //Factory Function - When we need to do same operation but do not want to fix issues all places create a function
-function createCircle() {
-    const mastercircle = {
-        radius: 1,
-        location: {
-            x: 1,
-            y: 1
-        },
-        sVisible: true,
-        draw: function () {
+// function createCircle(rad, loc) {
+//     return {
+//         radius: rad,
+//         location: loc,
+//         sVisible: true,
+//         draw: function () {
+//             console.log('draw');
+//         }
+//     };
+
+// }
+
+function createCircle(rad) {
+    return {
+        rad,
+        draw() {
             console.log('draw');
         }
     };
-
-
 }
+
+const circle = createCircle(1);
+console.log(circle);
